607e5a7a1e9a85914325507fa75a4a3168d3ba65
Dusan <dusanslankamenac8@gmail.com>
2026-03-28 18:08:53 +0100

    Promenjeni atributi u klub + korisnik
    - Klub dobija Stadion, BrojIgraca & BrojOsvojenihTitula dok Korisnik gubi ime i prezime

--- izvorni kod/SlojPodataka/KlasePodataka/Klub.cs
+++ izvorni kod/SlojPodataka/KlasePodataka/Klub.cs
@@ -12,7 +12,10 @@ namespace SlojPodataka.KlasePodataka
         public int KlubID { get; set; }
         public string NazivKluba { get; set; } = string.Empty;
         public string Grad { get; set; } = string.Empty;
+        public string Stadion { get; set; } = string.Empty;
         public string Adresa { get; set; } = string.Empty;
+        public int BrojIgraca { get; set; }
+        public int BrojOsvojenihTitula { get; set; }
         public int GodinaOsnivanja { get; set; }
         public ICollection<Zapisnik> DomacinUtakmice { get; set; } = new List<Zapisnik>();
         public ICollection<Zapisnik> GostUtakmice { get; set; } = new List<Zapisnik>();
