708bcd8eb355c764ee7083cc05d868ff1ebf18cc
Dusan <dusanslankamenac8@gmail.com>
2026-03-29 14:36:31 +0200

    Dodavanje mappera
    Poseban mapper za DTOs za sledece:
    - Klub
    - Stavka
    - Zapisnik

--- izvorni kod/SlojServisa/KlaseMapiranja/KlubMapper.cs
+++ izvorni kod/SlojServisa/KlaseMapiranja/KlubMapper.cs
@@ -1,6 +1,43 @@
-﻿namespace SlojServisa.KlaseMapiranja
+﻿using SlojPodataka.KlasePodataka;
+using SlojServisa.DTO;
+
+namespace SlojServisa.KlaseMapiranja
 {
     public class KlubMapper
     {
+        public KlubDTO UEntitet(Klub klub)
+        {
+            return new KlubDTO
+            {
+                KlubID = klub.KlubID,
+                NazivKluba = klub.NazivKluba,
+                Grad = klub.Grad,
+                Stadion = klub.Stadion,
+                Adresa = klub.Adresa,
+                BrojIgraca = klub.BrojIgraca,
+                BrojOsvojenihTitula = klub.BrojOsvojenihTitula,
+                GodinaOsnivanja = klub.GodinaOsnivanja
+            };
+        }
+
+        public Klub UDTO(KlubDTO dto)
+        {
+            return new Klub
+            {
+                KlubID = dto.KlubID,
+                NazivKluba = dto.NazivKluba,
+                Grad = dto.Grad,
+                Stadion = dto.Stadion,
+                Adresa = dto.Adresa,
+                BrojIgraca = dto.BrojIgraca,
+                BrojOsvojenihTitula = dto.BrojOsvojenihTitula,
+                GodinaOsnivanja = dto.GodinaOsnivanja
+            };
+        }
+
+        public List<KlubDTO> UListuDTO(List<Klub> klubovi)
+        {
+            return klubovi.Select(k => UEntitet(k)).ToList();
+        }
     }
 }
---
+++ izvorni kod/SlojServisa/KlaseMapiranja/StavkaMapper.cs
@@ -0,0 +1,33 @@
+﻿using SlojPodataka.KlasePodataka;
+using SlojServisa.DTO;
+
+namespace SlojServisa.KlaseMapiranja
+{
+    public class StavkaMapper
+    {
+        public StavkaDTO UDTO(StavkaZapisnika stavka)
+        {
+            return new StavkaDTO
+            {
+                StavkaID = stavka.StavkaID,
+                ZapisnikID = stavka.ZapisnikID,
+                MinutGola = stavka.MinutGola,
+                ImeStrelca = stavka.ImeStrelca,
+                TimID = stavka.TimID,
+                NazivTima = stavka.Tim?.NazivKluba ?? string.Empty
+            };
+        }
+
+        public StavkaZapisnika UEntitet(StavkaDTO dto)
+        {
+            return new StavkaZapisnika
+            {
+                StavkaID = dto.StavkaID,
+                ZapisnikID = dto.ZapisnikID,
+                MinutGola = dto.MinutGola,
+                ImeStrelca = dto.ImeStrelca,
+                TimID = dto.TimID
+            };
+        }
+    }
+}
