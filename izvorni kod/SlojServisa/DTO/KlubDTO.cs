8d8ca183beb648451df2b97094b60d91d550ced2
Dusan <dusanslankamenac8@gmail.com>
2026-03-29 14:26:46 +0200

    Dodati DTOs
    DTOs su dodani za sledece:
    - Klub
    - Stavka zapisnika
    - Zapisnik

--- izvorni kod/SlojServisa/DTO/KlubDTO.cs
+++ izvorni kod/SlojServisa/DTO/KlubDTO.cs
@@ -2,5 +2,13 @@
 {
     public class KlubDTO
     {
+        public int KlubID { get; set; }
+        public string NazivKluba { get; set; } = string.Empty;
+        public string Grad { get; set; } = string.Empty;
+        public string Stadion { get; set; } = string.Empty;
+        public string Adresa { get; set; } = string.Empty;
+        public int BrojIgraca { get; set; }
+        public int BrojOsvojenihTitula { get; set; }
+        public int GodinaOsnivanja { get; set; }
     }
 }
