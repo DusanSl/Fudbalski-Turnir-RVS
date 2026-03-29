461d61d15ac0e086db3af5743f0a0bd26597eeaf
Dusan <dusanslankamenac8@gmail.com>
2026-03-29 15:28:04 +0200

    Dodani restkontroleri
    Rest kontroleri su dodani za Klub & Zapisnik. Takodje su ubacena ogranicenja u program.cs

--- izvorni kod/SlojServisa/Program.cs
+++ izvorni kod/SlojServisa/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SlojPodataka.TehnoloskeKlase;
+using SlojPoslovneLogike.Ogranicenja;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<TurnirDbContext>(options =>
     options.UseSqlServer(builder.Configuration
         .GetConnectionString("PodrazumevanaKonekcija")));
+
+builder.Services.AddScoped<ZapisnikRepozitorijum>();
+builder.Services.AddScoped<CitacPravila>(provider =>
+    new CitacPravila(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+        "Ogranicenja", "pravila_hronologije.xml")));
 
 var app = builder.Build();
 
