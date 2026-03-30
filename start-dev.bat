22efe84c8058f26ffb3411f03dbe7fcba67fa26d
Dusan <dusanslankamenac8@gmail.com>
2026-03-30 16:53:17 +0200

    Promena .bat skripte
    .bat skripta je promenjena da moze da se koristi hot reload.

--- start-dev.bat
+++ start-dev.bat
@@ -1,3 +1,3 @@
 @echo off
-start "Servis" cmd /k "cd "izvorni kod\SlojServisa" && dotnet run"
-start "Prezentacija" cmd /k "cd "izvorni kod\PrezentacioniSloj" && dotnet run"
+start "Servis" cmd /k "cd "izvorni kod\SlojServisa" && dotnet watch run"
+start "Prezentacija" cmd /k "cd "izvorni kod\PrezentacioniSloj" && dotnet watch run"
