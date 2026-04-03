166418239b6ea01a1016fdf736d224653181a675
Dusan <dusanslankamenac8@gmail.com>
2026-04-03 22:31:17 +0200

    Dodavanje limita na podatke
    Dodani su limiti na podatke npr. [Required] & [StringLength(x)], ili Range na odredjene podatke.

--- izvorni kod/SlojPodataka/KlasePodataka/Klub.cs
+++ izvorni kod/SlojPodataka/KlasePodataka/Klub.cs
@@ -1,4 +1,4 @@
-﻿using SlojPodataka.KlasePodataka;
+using SlojPodataka.KlasePodataka;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,12 +12,23 @@ namespace SlojPodataka.KlasePodataka
     {
         [Key]
         public int KlubID { get; set; }
+        [Required]
+        [StringLength(100)]
         public string NazivKluba { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50)]
         public string Grad { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100)]
         public string Stadion { get; set; } = string.Empty;
+        [Required]
+        [StringLength(200)]
         public string Adresa { get; set; } = string.Empty;
+        [Required]
         public int BrojIgraca { get; set; }
+        [Required]
         public int BrojOsvojenihTitula { get; set; }
+        [Required]
         public int GodinaOsnivanja { get; set; }
         public ICollection<Zapisnik> DomacinUtakmice { get; set; } = new List<Zapisnik>();
         public ICollection<Zapisnik> GostUtakmice { get; set; } = new List<Zapisnik>();
